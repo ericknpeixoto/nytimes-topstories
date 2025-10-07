6c31099f7749b117c028a2dd160fdcfdb58591aa
ericknpeixoto <ericknpeixoto>
2025-10-07 00:34:07 +0000

    Refactoring: Weekly Trending Persons
    Refactoring the ingestion of the gold table Weekly Trending Persons. It will serve as a model for the other tables

---
+++ src/libs/utils.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+
+def table_exists(spark, catalog, schema_name, table_name):
+    spark.catalog.setCurrentCatalog(catalog)
+    return spark.catalog.tableExists(f'{schema_name}.{table_name}')
+
+
+def get_first_partition(spark, catalog, schema_name, table_name, partition):
+    spark.catalog.setCurrentCatalog(catalog)
+    return spark.sql(f'SHOW PARTITIONS {catalog}.{schema_name}.{table_name}').orderBy('{partition}').limit(1).collect()[0][0]
+
+def get_last_partition(spark, catalog, schema_name, table_name, partition):
+    spark.catalog.setCurrentCatalog(catalog)
+    return spark.sql(f'SHOW PARTITIONS {catalog}.{schema_name}.{table_name}').orderBy('{partition}', ascending = False).limit(1).collect()[0][0]
